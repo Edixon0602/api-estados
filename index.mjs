2c5a880e9fe53b325e517787656190deffd91abf
Edixon0602 <e.serrano0602@gmail.com>
2024-05-24 21:54:52 -0400

    add: cors support

--- index.mjs
+++ index.mjs
@@ -1,10 +1,12 @@
 import express from 'express';
 import indexRouter from './routes/index.mjs';
+import cors from 'cors'
 
 const port = process.env.PORT || 3000
 const app = express();
 
 app.use(express.static('public'));
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
